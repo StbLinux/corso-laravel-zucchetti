130acaed41e5022c10068d95788f58fefaa6b27f
Sidrit Trandafili <forge405@gmail.com>
2018-07-09 12:36:03 +0200

    sessione 1 1/2

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,13 @@
 <?php
 
-// app riceve richiesta
-// viene interpretata nel router
-// controller - > view
 
 Route::get('/', function () {
-    // $tasks = [
-    //     'impara laravel',
-    //     'usa laravel',
-    //     'ripeti',
-    // ];
-    //resources/views/welcome.blade.php
-
     $tasks = DB::table('tasks')->get();
-
-    // return $tasks;
-
     return view('welcome', compact('tasks'));
+});
 
-    // $utente = 'Sid';
+Route::get('tasks/{id}', function ($id) {
+    $task = DB::table('tasks')->find($id);
 
-    // return view('welcome', compact('utente'));
-    // return view('welcome')->withUtente($utente);
-    // return view('welcome')->with(['utente' => $utente]);
-    // return view('welcome', ['utente' => $utente]);
+    return json_decode(json_encode($task), true);
 });
