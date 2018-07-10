5f4947436dd12435e57adf5a87685080e661c653
Sidrit Trandafili <forge405@gmail.com>
2018-07-10 11:30:33 +0200

    sessione 2 2/

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    $title = $faker->sentence;
+
+    return [
+        'title' => $title,
+        'slug' => str_slug($title),
+        'preview' => $faker->paragraph,
+        'body' => $faker->paragraphs(7, true),
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'category_id' => function () {
+            return factory(App\Category::class)->create()->id;
+        },
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // 20 Category
+        $categories = factory(App\Category::class, 20)->create();
+        // 40 Tag
+        $tags = factory(App\Tag::class, 40)->create();
+
+        // 10 User
+        $users = factory(App\User::class, 10)->create();
+
+        // Ciascun User 15 Post
+        foreach ($users as $user) {
+            // Ciascun Post 1 Category random tra quelle create
+            $posts = factory(App\Post::class, 15)->create([
+                'user_id' => $user->id,
+                'category_id' => $categories->random()->id,
+            ]);
+
+            foreach ($posts as $post) {
+                // Ciascun Post 3 Tag random tra quelli creati
+                // prendere 3 tag random
+                // estrarre id di quest 3 random tag
+                // associare ciascun id al post
+                $post->tags()->sync($tags->random(3)->pluck('id')->toArray());
+            }
+        }
     }
 }
